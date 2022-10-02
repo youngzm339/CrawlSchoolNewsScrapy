9221ab4d13ba5fad57c6676bc5548070791c5363
YoungZM <youngzm339@outlook.com>
2022-10-02 23:58:30 +0800

    1st version without github action

--- schoolnews_haut/schoolnews_haut/spiders/xsdt-news.py
+++ schoolnews_haut/schoolnews_haut/spiders/xsdt-news.py
@@ -3,9 +3,10 @@ from schoolnews_haut.items import SchoolnewsHautItem
 
 
 class NewsSpider(scrapy.Spider):
-    name = 'news'
+    name = 'xsdt-news'
+    # 学术动态
     allowed_domains = ['www.haut.edu.cn']
-    start_urls = ['https://www.haut.edu.cn/xwzx/tzgg.htm']
+    start_urls = ['https://www.haut.edu.cn/xwzx/xsdt.htm']
 
     def parse(self, response):
         item = SchoolnewsHautItem()
@@ -16,10 +17,10 @@ class NewsSpider(scrapy.Spider):
             else:
                 item['news_name'] = response.xpath(
                     '/html/body/div/div[2]/div[2]/div[2]/div/div/ul/li[%d]/a/text()' % (i)).extract_first()
-                item['news_href'] = response.xpath(
-                    '/html/body/div/div[2]/div[2]/div[2]/div/div/ul/li[%d]/a/@href' % (i)).extract_first()
+                item['news_href'] = response.urljoin(response.xpath(
+                    '/html/body/div/div[2]/div[2]/div[2]/div/div/ul/li[%d]/a/@href' % (i)).extract_first())
                 item['news_date'] = response.xpath(
-                    '/html/body/div/div[2]/div[2]/div[2]/div/div/ul/li[%d]/span/text()' % (i)).extract_first()
+                    '/html/body/div/div[2]/div[2]/div[2]/div/div/ul/li[%d]/span/text()' % (i)).extract_first()[5:]
                 yield item
                 i += 1
         pass
